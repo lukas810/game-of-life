133f8804fd820831c2a0623b81f79cd938fcd033
lukas810 <lukas.puff38@gmail.com>
2024-03-25 12:33:21 +0100

    added basic game mechanic

---
+++ src/main/java/Window.java
@@ -0,0 +1,54 @@
+package main.java;
+
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
+
+//import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.WindowConstants;
+
+public class Window extends JFrame {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private GamePanel gamePanel;
+
+	private JPanel mainPanel;
+
+	public Window() {
+
+		add(getMainPanel());
+		setSize(640, 510);
+		setTitle("Conway's Game of Life");
+		setLocationRelativeTo(null);
+		setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+		setVisible(true);
+
+	}
+
+	private JPanel getMainPanel() {
+		if (mainPanel == null) {
+			mainPanel = new JPanel(new GridBagLayout());
+			mainPanel.add(getGamePanel(), new GridBagConstraints(0, 0, 1, 1, 1, 1, GridBagConstraints.CENTER,
+					GridBagConstraints.BOTH, new Insets(0, 0, 0, 0), 0, 0));
+	//		JButton b = new JButton("Test");
+	//		b.addActionListener(e -> gamePanel.setRunning(!gamePanel.isRunning()));
+	//		mainPanel.add(b, new GridBagConstraints(0, 1, 1, 1, 1, 1, GridBagConstraints.CENTER,
+	//				GridBagConstraints.BOTH, new Insets(0, 0, 0, 0), 0, 0));
+			gamePanel.setRunning(true);
+		}
+
+		return mainPanel;
+	}
+
+	private GamePanel getGamePanel() {
+		if (gamePanel == null) {
+			gamePanel = new GamePanel(640, 480);
+		}
+		return gamePanel;
+	}
+}
